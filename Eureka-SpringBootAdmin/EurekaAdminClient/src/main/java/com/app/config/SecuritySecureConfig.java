4e2aef71e9e6a10397c2e2412f7ad97a336ce06f
zhangchaohuiget <zhangchaohuiget@163.com>
2021-06-18 10:43:18 +0800

    1

---
+++ Eureka-SpringBootAdmin/EurekaAdminClient/src/main/java/com/app/config/SecuritySecureConfig.java
@@ -0,0 +1,26 @@
+package com.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+@Configuration
+public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+
+        http.authorizeRequests()
+                // 此路径需要加验证
+                .antMatchers("/actuator/**").authenticated()
+                // 其他路径放开
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic().and()
+                .csrf().disable();
+    }
+}
+
